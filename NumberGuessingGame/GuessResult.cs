836022fad2c349061fe67770c682c7f913d2ff16
Sivuyile Mtwetwe <sivuyilemtwetwe@gmail.com>
2026-07-07 14:20:14 +0200

    Created a class with the logic for validating a guess from a user.

---
+++ NumberGuessingGame/GuessResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    internal enum GuessResult
+    {
+        tooHigh,
+        tooLow,
+        correct,
+        outOfLimits
+    }
+}
---
+++ NumberGuessingGame/NumberGuessing.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberGuessingGame
+{
+    internal class NumberGuessing
+    {
+        private int randomNumber = 0;
+        private int maximum = 0;
+        private int minimum = 0;
+
+        public void generateRandomNumber(int minimum, int maximum)
+        {
+            Random rand = new Random();
+
+            randomNumber = rand.Next( minimum, maximum);
+
+            this.minimum = minimum;
+            this.maximum = maximum;
+
+            //return randomNumber;
+
+        }
+
+        public NumberGuessing(int minimum, int maximum)
+        {
+            generateRandomNumber(minimum, maximum);
+        }
+         
+
+        public GuessResult makeGuess(int guess)
+        {
+            if( guess > maximum)
+            {
+                return GuessResult.outOfLimits;
+            }else if (guess < minimum)
+            {
+                return GuessResult.outOfLimits;
+
+            }
+            else
+            {
+                if (guess == randomNumber)
+                {
+
+
+                    return GuessResult.correct;
+
+                }
+                else if (guess > randomNumber)
+                {
+                    return GuessResult.tooHigh;
+                }
+                else
+                {
+                    return GuessResult.tooLow;
+                }
+            }
+
+            
+        }
+    }
+}
